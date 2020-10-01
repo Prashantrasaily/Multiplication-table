bf5d8d3a41b3374271beadf6bf86dfb835b08616
Sita833 <72214892+Sita833@users.noreply.github.com>
2020-10-01 21:09:16 +0530

    added comments to the python code .

--- Multiplication.py
+++ Multiplication.py
@@ -1,3 +1,3 @@
-m=int(input('Enter number: ' )
-for i in range (1, 11):
-    print(m, '×', i, '=', m× i)
+m=int(input('Enter number: ' ) # asking for input from user 
+for i in range (1, 11): # running a for loop from 1 to 10
+    print(m, '×', i, '=', m× i) # print the multiplication in the desired format
